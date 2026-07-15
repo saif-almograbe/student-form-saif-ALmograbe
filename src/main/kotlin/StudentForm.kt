a444d27f07d9fa3047e51b1cc62804581f8cd2c3
saif-almograbe <120220622154@st.ahu.edu.jo>
2026-07-15 12:33:37 +0300

    full stud

--- src/main/kotlin/StudentForm.kt
+++ src/main/kotlin/StudentForm.kt
@@ -1,6 +1,6 @@
 data class StudentForm(
-    var name: String = "",
-    var lastName: String = "",
-    var age: String = "",
-    var university: String = "",
-)
+    var name: String = "saif ",
+    var lastName: String = "almograbe",
+    var age: String = "22",
+    var university: String = "ahu",
+     )
